397866c66838831ed7e72328b08d3a9b3ecdd475
LukasFrmm <lukasfreimonas@gmail.com>
2020-03-10 12:48:02 +0200

    Updates

---
+++ precache-manifest.a8eb504c9549111383b2f921647b88bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c487dc323e75e4ec6f36bef4565ec3",
+    "url": "/roulette_game/index.html"
+  },
+  {
+    "revision": "a3688ff19587cc1e0e24",
+    "url": "/roulette_game/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "2ea40dd6e1ef0ee2a26b",
+    "url": "/roulette_game/static/css/main.c1a4a060.chunk.css"
+  },
+  {
+    "revision": "a3688ff19587cc1e0e24",
+    "url": "/roulette_game/static/js/2.5c9da054.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/roulette_game/static/js/2.5c9da054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea40dd6e1ef0ee2a26b",
+    "url": "/roulette_game/static/js/main.36b4d35b.chunk.js"
+  },
+  {
+    "revision": "9c00a14cc208241df215",
+    "url": "/roulette_game/static/js/runtime-main.5761d728.js"
+  }
+]);
